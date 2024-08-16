11e46b89b3ada4be67827291e7bc3149956090f5
TheDeveloper <gabriel4321hh@gmail.com>
2024-08-16 17:33:41 -0300

    Profile estilizado , titulos estilizados

--- src/Components/Intro/Intro.tsx
+++ src/Components/Intro/Intro.tsx
@@ -8,13 +8,13 @@ export function Intro(){
                
                 <Profile/>
                 <div className="my-4 lg:m-0 ">
-                    <span className="text-1xl text-start lg:text-2xl font-medium lg:ml-2">Olá, meu nome é</span><br />
-                    <span className="text-4xl lg:text-8xl font-extrabold">Gabriel Barbosa,</span>
-                    <h1 className="text-2xl lg:text-4xl font-bold">Desenvolvedor Front end</h1>
+                    <span className="text-1xl text-start lg:text-2xl font-medium lg:ml-2 text-black ">Olá, meu nome é</span><br />
+                    <span className="text-4xl lg:text-8xl font-extrabold bg-gradient-to-r from-lime-500 via-green-500 to-cyan-400 bg-clip-text text-transparent">Gabriel Barbosa,</span>
+                    <h1 className="text-2xl lg:text-4xl font-bold bg-gradient-to-r from-green-400 via-yellow-400 to-green-500 bg-clip-text text-transparent ">Desenvolvedor Front end</h1>
                 </div>
                 <div className="flex gap-8 p-10">
-                    <button className="border border-green-400 rounded py-3 px-10">Ver Projetos</button>
-                    <button className="border border-black rounded py-3 px-10 ">Baixar CV</button>
+                    <button className="border-2 border-green-400 rounded py-3 px-10">Ver Projetos</button>
+                    <button className="border-2 border-black rounded py-3 px-10 ">Baixar CV</button>
                 </div>
             </div>
         </section>
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Header } from './Components/Header/Header.tsx'
-import './index.css'
 import { Intro } from './Components/Intro/Intro.tsx'
 import { AboutMe } from './Components/AboutMe/AboutMe.tsx'
 import { Footer } from './Components/Footer/Footer.tsx'
+import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
